e4d66b2d4015c4d2c1b5584f04e9a1541202d782
korei999 <ju7t1xe@gmail.com>
2024-12-17 17:58:57 +0200

    Heap methods

--- src/HeapTest.cc
+++ src/HeapTest.cc
@@ -11,4 +11,17 @@ main()
     Arena arena(SIZE_1K);
     defer( arena.freeAll() );
 
+    Heap<long> h(&arena.super);
+    defer( h.destroy(&arena.super) );
+
+    h.pushMax(&arena.super, 2);
+    h.pushMax(&arena.super, 4);
+    h.pushMax(&arena.super, 1);
+    h.pushMax(&arena.super, -2);
+    h.pushMax(&arena.super, 8);
+
+    LOG("h: [{}]\n", h.vec);
+
+    while (h.vec.getSize() > 0)
+        LOG("max: {}\n", h.maxExtract());
 }
