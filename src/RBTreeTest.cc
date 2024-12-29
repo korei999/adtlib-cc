0567a74d5f54fa2f3e64ff3ae75152dc73852e95
korei999 <ju7t1xe@gmail.com>
2024-12-29 13:41:05 +0200

    RBTree: add emplace

--- src/RBTreeTest.cc
+++ src/RBTreeTest.cc
@@ -18,18 +18,19 @@ main()
     RBTree<long> tree(&al);
     defer( tree.destroy() );
 
-    tree.insert(1L, false);
-    tree.insert(-1L, false);
-    tree.insert(2L, false);
-    tree.insert(-2L, false);
-    tree.insert(-3L, false);
-    tree.insert(-6L, false);
-    tree.insert(10L, false);
-    tree.insert(22L, false);
+    tree.emplace(false, 1L);
+    tree.emplace(false, -1L);
+    tree.emplace(false, 2L);
+    tree.emplace(false, -2L);
+    tree.emplace(false, -3L);
+    tree.emplace(false, -6L);
+    tree.emplace(false, 10L);
+    tree.emplace(false, 22L);
 
     tree.removeAndFree(-3L);
+    tree.removeAndFree(-6L);
 
     LOG_GOOD("root: {}\n", *tree.getRoot());
 
-    RBPrintNodes(&arena, tree.getRoot(), stdout, "", false);
+    RBPrintNodes(&arena, tree.getRoot(), stdout);
 }
