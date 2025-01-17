1a46c8985a4c6de3ee900f0dc3c7a85c0fd5b5a1
korei999 <ju7t1xe@gmail.com>
2025-01-18 01:37:56 +0200

    add ScratchBuffer class

--- src/ThreadPoolTest.cc
+++ src/ThreadPoolTest.cc
@@ -2,17 +2,22 @@
 #include "adt/logs.hh"
 #include "adt/ThreadPool.hh"
 #include "adt/BufferAllocator.hh"
-#include "adt/Arena.hh"
+#include "adt/ScratchBuffer.hh"
 
 using namespace adt;
 
-thread_local static Arena tls_Arena(SIZE_1K);
+thread_local static u8 tls_aBuff[2048] {};
+thread_local static ScratchBuffer tls_Scratch(Span{tls_aBuff});
 
 static int
 task(void* pArg)
 {
-    char* pBuff = (char*)tls_Arena.malloc(1, 500);
+    int size = 2048 / 4;
+    auto spBuff = tls_Scratch.getZMem<char>(size);
+    print::toBuffer(spBuff.data(), spBuff.getSize() - 1, "must not corrupt this string");
     utils::sleepMS(*(f64*)pArg);
+
+    assert(String(spBuff.data()) == "must not corrupt this string");
 
     LOG("done waiting for {}\n", *(f64*)pArg);
     return thrd_success;
@@ -43,25 +48,11 @@ main()
     defer( lock.destroy() );
 
     tp.submit(task, &ms);
-    tp.submit(task, &ms);
-    tp.submit(task, &ms);
-    tp.submit(task, &ms);
-    tp.submit(task, &ms);
-    tp.submit(task, &ms);
-    tp.submit(task, &ms);
-    tp.submit(task, &ms);
-    tp.submit(task, &ms);
-    tp.submit(task, &ms);
-    tp.submit(task, &ms);
-    tp.submit(task, &ms);
-    tp.submit(task, &ms);
-    tp.submit(task, &ms);
-    tp.submit(task, &ms);
-    tp.submit(task, &ms);
-    tp.submit(task, &ms);
-    tp.submit(task, &ms);
     tp.submitSignal(signaled, {}, &lock);
     tp.submit(task, &ms);
+
+    for (ssize i = 0; i < 100; ++i)
+        tp.submit(task, &ms);
 
     tp.wait();
 
