30fe468c2e40275ed936bbcb58f5a0254c509270
korei999 <ju7t1xe@gmail.com>
2025-05-17 02:19:48 +0300

    StringFixed tests

---
+++ src/StringFixed.cc
@@ -0,0 +1,27 @@
+#include "adt/logs.hh"
+
+using namespace adt;
+
+int
+main()
+{
+    {
+        const char* nts = "1234567890ABCD";
+        StringFixed<10> sf10 {};
+        sf10 = nts;
+
+        ADT_ASSERT_ALWAYS(sf10.data()[9] == '\0', "");
+        ADT_ASSERT_ALWAYS(sf10 == "123456789", "");
+        LOG("sf: '{}'\n", sf10.data());
+    }
+
+    {
+        StringFixed<5> sf5 = "12345_ABCF";
+        StringFixed<5> sf5_2 = sf5;
+
+        ADT_ASSERT_ALWAYS(sf5.data()[4] == '\0', "");
+        ADT_ASSERT_ALWAYS(sf5 == sf5_2, "");
+        ADT_ASSERT_ALWAYS(sf5 == "1234", "");
+        LOG("sf5: '{}'\n", sf5);
+    }
+}
