7e1bd29d31626cd1a055f031befeeb1fa028c272
korei999 <ju7t1xe@gmail.com>
2025-07-24 19:33:06 +0300

    add QueueMPMC

--- src/QueueMPMC.cc
+++ src/QueueMPMC.cc
@@ -2,13 +2,46 @@
 #include "adt/Thread.hh"
 #include "adt/defer.hh"
 #include "adt/logs.hh"
+#include "adt/ThreadPool.hh"
 
 using namespace adt;
+
+constexpr isize BIG = 1 << 18;
+
+QueueMPMC<int, BIG> s_q {INIT};
+static atomic::Int s_atomCounter {};
 
 int
 main()
 {
     LOG_NOTIFY("QueueMPMC test...\n");
+
+    ThreadPool<128> tp {StdAllocator::inst()};
+    defer( tp.destroy(StdAllocator::inst()) );
+
+    auto clEnqueue = [&]
+    {
+        while (!s_q.push(1))
+            ;
+    };
+
+    auto clDequeue = [&]
+    {
+        Opt<int> r = s_q.pop();
+        if (r) s_atomCounter.fetchAdd(r.value(), atomic::ORDER::RELAXED);
+    };
+
+    for (isize i = 0; i < BIG; ++i)
+        tp.addLambdaRetry(clEnqueue);
+
+    tp.wait();
+
+    for (isize i = 0; i < BIG; ++i)
+        tp.addLambdaRetry(clDequeue);
+
+    tp.wait();
+
+    ADT_ASSERT_ALWAYS(s_atomCounter.load(atomic::ORDER::RELAXED) == BIG, "{}", s_atomCounter.load(atomic::ORDER::RELAXED));
 
     LOG_GOOD("QueueMPMC test passed.\n");
 }
