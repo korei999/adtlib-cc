6a3379c0314cfca386a2bbe3694b4ef90b3ec65f
korei999 <ju7t1xe@gmail.com>
2025-07-29 21:02:56 +0300

    print: tests

--- src/printTest.cc
+++ src/printTest.cc
@@ -48,7 +48,7 @@ main()
 
         char aBuff[64] {};
         for (isize i = 0; i < BIG; ++i)
-            print::toSpan(aBuff, "{}, {}, {}", i, i, float(i));
+            print::toSpan(aBuff, "{}, {}, {}, {}", i, i, f32(i), f64(i));
 
         // const auto t1 = utils::timeNowUS();
 
@@ -64,7 +64,7 @@ main()
 
         char aBuff[64] {};
         for (isize i = 0; i < BIG; ++i)
-            print::toSpan(aBuff, "{}, {}, {}", i, i, float(i));
+            print::toSpan(aBuff, "{}, {}, {}, {}", i, i, f32(i), f64(i));
 
         const auto t1 = utils::timeNowUS();
 
@@ -80,7 +80,7 @@ main()
 
         char aBuff[64] {};
         for (isize i = 0; i < BIG; ++i)
-            snprintf(aBuff, sizeof(aBuff) - 1, "%lld, %lld, %g", i, i, float(i));
+            snprintf(aBuff, sizeof(aBuff) - 1, "%lld, %lld, %f, %lf", i, i, f32(i), f64(i));
 
         const auto t1 = utils::timeNowUS();
 
@@ -96,7 +96,7 @@ main()
 
         char aBuff[64] {};
         for (isize i = 0; i < BIG; ++i)
-            std::format_to(aBuff, "{}, {}, {}", i, i, float(i));
+            std::format_to(aBuff, "{}, {}, {}, {}", i, i, f32(i), f64(i));
 
         const auto t1 = utils::timeNowUS();
         // print::out("aBuff: {}\n", aBuff);
