07fdc8bc213ed018e66ae49c314f8b44208531b2
korei999 <ju7t1xe@gmail.com>
2025-08-03 00:20:23 +0300

    simplified reference counter

---
+++ src/RefCount.cc
@@ -0,0 +1,99 @@
+#include "adt/logs.hh"
+#include "adt/RefCount.hh"
+#include "adt/math.hh"
+#include "adt/Arena.hh"
+#include "Types.hh"
+
+using namespace adt;
+
+template<typename T>
+static void
+show(RefCountedPtr<T> rcp)
+{
+    if (!rcp)
+    {
+        LOG_WARN("!rcp\n");
+        return;
+    }
+    RefGuard refGuard {&rcp};
+
+    LOG_GOOD("rcp: {}\n", *rcp);
+}
+
+int
+main()
+{
+    LOG_NOTIFY("RefCount test...\n");
+
+    {
+        RefCountedPtr<int> rcp = RefCountedPtr<int>::alloc(1);
+        defer( rcp.unref() );
+
+        RefCountedPtr<int> rcp2 = rcp.ref();
+        defer( rcp.unref() );
+
+        show(rcp);
+
+        *rcp = 2;
+        ADT_ASSERT_ALWAYS(*rcp == *rcp2, "{}, {}", *rcp, *rcp2);
+
+        show(rcp2);
+    }
+
+    {
+        WeakPtr<What> wp;
+        defer( wp.weakUnref() );
+
+        {
+            auto rcp = RefCountedPtr<What>::alloc(1);
+            defer( rcp.unref() );
+
+            RefCountedPtr<What> rcp2 = rcp.ref();
+            defer( rcp.unref() );
+
+            wp = rcp2.weakRef();
+
+            show(rcp2);
+
+            show(wp);
+        }
+
+        show(wp);
+    }
+
+    {
+        RefCountedPtr<math::V3> rcp = StdAllocator::inst()->alloc<math::V3>(math::V3{1.1f, 2.2f, 3.3f});
+        defer( rcp.unref() );
+
+        auto rcp2 = rcp.ref();
+        defer( rcp2.unref() );
+
+        show(rcp);
+
+        *rcp = math::V3{6.6f, 7.7f, 8.8f};
+        ADT_ASSERT_ALWAYS(*rcp2 == *rcp, "{}, {}", *rcp2, *rcp);
+
+        show(rcp2);
+    }
+
+    {
+        auto clDeleter = [](Arena* p) noexcept {
+            p->freeAll();
+        };
+
+        auto rcpArena = RefCountedPtr<Arena>::allocWithDeleter(clDeleter, Arena{SIZE_1K});
+        defer( rcpArena.unref() );
+    }
+
+    {
+        auto clDeleter = [](Arena* p) noexcept {
+            p->freeAll();
+            ::free(p);
+        };
+
+        RefCountedPtr<Arena> rpcArena {clDeleter, StdAllocator::inst()->alloc<Arena>(SIZE_1K)};
+        defer( rpcArena.unref() );
+    }
+
+    LOG_GOOD("RefCount test passed\n");
+}
