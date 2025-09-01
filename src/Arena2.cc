75997b901fea242b4231674b923937228e09686b
korei999 <ju7t1xe@gmail.com>
2025-09-01 08:09:39 +0300

    Arena: better owned pointers

--- src/Arena2.cc
+++ src/Arena2.cc
@@ -48,44 +48,51 @@ main()
 
         {
             static int i = 0;
+
+            struct Destructive
             {
-                struct Destructive
+                int m_i;
+                Destructive() noexcept { m_i = ++i; LogDebug{"m_i: {}\n", m_i}; }
+
+                ~Destructive() noexcept
                 {
-                    int m_i;
-                    Destructive() noexcept { m_i = ++i; LogDebug{"m_i: {}\n", m_i}; }
-
-                    ~Destructive() noexcept
-                    {
-                        LogDebug{"{} dies...\n", m_i};
-                        --i;
-                    };
-
-                    void sayHi() noexcept { LogDebug{"{} says hi\n", m_i}; }
+                    LogDebug{"{} dies...\n", m_i};
+                    --i;
                 };
 
-                Arena::Owned<Destructive> pp0 = arena.allocOwnedWithDeleter<Destructive>([](Arena*, void* pD) {
-                    auto& r = (*(Destructive*)pD);
-                    LogWarn("({}) custom deleter in top frame\n", r.m_i);
-                    r.~Destructive();
-                });
-                pp0->sayHi();
+                void sayHi() noexcept { LogDebug{"{} says hi\n", m_i}; }
+            };
 
-                LogWarn("offset before push: {}\n", arena.m_off);
+            Arena::Ptr<Destructive> pp;
+
+            {
                 ArenaStateGuard pushed {&arena};
 
-                Arena::Owned<Destructive> pp1 = arena.allocOwned<Destructive>();
-                Arena::Owned<Destructive> pp2 = arena.allocOwned<Destructive>();
-                Arena::Owned<Destructive> pp3 = arena.allocOwnedWithDeleter<Destructive>([](Arena*, void* pD) {
-                    auto& r = (*(Destructive*)pD);
-                    LogDebug("({}) custom deleter\n", r.m_i);
-                    r.~Destructive();
-                });
+                new(&pp) Arena::Ptr<Destructive> {&arena};
+                arena.addToDestructList(&pp);
 
+                Arena::Ptr<Destructive> pp0 {&arena};
+                arena.addToDestructList(&pp0);
+
+                Arena::Ptr<Destructive> pp1 {&arena};
+                arena.addToDestructList(&pp1);
+
+                Arena::Ptr<Destructive> pp2 {&arena};
+                arena.addToDestructList(&pp2);
+
+                pp0->sayHi();
                 pp1->sayHi();
                 pp2->sayHi();
-                pp3->sayHi();
             }
             LogDebug("offset after pop: {}\n", arena.m_off);
+
+            Arena::Ptr<Destructive> pp3 {&arena};
+            arena.addToDestructList(&pp3);
+
+            pp3->sayHi();
+
+            ADT_ASSERT_ALWAYS(!(bool)pp, "(bool)pp: {}\n", (bool)pp);
+            if (pp) pp->sayHi();
 
             ADT_ASSERT_ALWAYS(i == 1, "i: {}\n", i);
         }
