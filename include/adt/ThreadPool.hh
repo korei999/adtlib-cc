bae3bd62eb18bc32747a175d8c22caed5e235e98
korei999 <ju7t1xe@gmail.com>
2025-09-03 18:58:32 +0300

    whitespace

--- include/adt/ThreadPool.hh
+++ include/adt/ThreadPool.hh
@@ -278,7 +278,7 @@ ThreadPool::ThreadPool(
     void (*pfnOnLoopStart)(void*), void* pLoopStartArg,
     void (*pfnOnLoopEnd)(void*), void* pLoopEndArg,
     isize qSize,
-    isize arenaReserve, 
+    isize arenaReserve,
     int nThreads
 )
     : m_spThreads(StdAllocator::inst()->zallocV<Thread>(nThreads), nThreads),
