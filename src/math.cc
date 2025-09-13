f2afe8a042f07a4c5bdcfad8c8611e36e51420c4
korei999 <ju7t1xe@gmail.com>
2025-09-13 14:19:08 +0300

    math operators

--- src/math.cc
+++ src/math.cc
@@ -1,8 +1,15 @@
 #include "adt/Logger.hh"
+#include "adt/math.hh"
 
 using namespace adt;
 
 int
 main()
 {
+    math::V2 v0 {1, 2};
+    math::V2 v1 = 3.3f * v0;
+    math::M3 m3_0 = math::M3Iden();
+    math::V3 v3_0 {};
+
+    m3_0 * v3_0;
 }
