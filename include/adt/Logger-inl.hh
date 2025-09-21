8ca5e5846933e6ff36377580c8872ebcbe65f982
korei999 <ju7t1xe@gmail.com>
2025-09-21 15:06:06 +0300

    unused

--- include/adt/Logger-inl.hh
+++ include/adt/Logger-inl.hh
@@ -5,15 +5,6 @@
 #include <source_location>
 #include <utility>
 
-#define ADT_LOGGER_COL_NORM  "\x1b[0m"
-#define ADT_LOGGER_COL_RED  "\x1b[31m"
-#define ADT_LOGGER_COL_GREEN  "\x1b[32m"
-#define ADT_LOGGER_COL_YELLOW  "\x1b[33m"
-#define ADT_LOGGER_COL_BLUE  "\x1b[34m"
-#define ADT_LOGGER_COL_MAGENTA  "\x1b[35m"
-#define ADT_LOGGER_COL_CYAN  "\x1b[36m"
-#define ADT_LOGGER_COL_WHITE  "\x1b[37m"
-
 namespace adt
 {
 
